ec687ec7ae15c97878fd9cf42374f03732ca288b
SibaDoge <38933716+SibaDoge1@users.noreply.github.com>
2021-11-02 23:59:31 +0900

    [feat]: 댓글, 대댓글 구현, 로그인버그 수정 (#32)
    * [feet]: (WIP) 댓글, 대댓글 일부 구현, 로그인버그 수정
    
    1. 댓글 api 6가지 완성
    3. 로그인 버그 수정
    4. 댓글을 위해 모든 모델 변경

---
+++ src/service/commentService.js
@@ -0,0 +1,94 @@
+import Comment from '../models/commentModel'
+import Post from '../models/postModel'
+const { ObjectId } = require('mongodb');
+
+export const readCommentsInPost = async (postId, offset, limit) => {
+  try {
+    const comments = await Comment.find({
+        postId:postId
+    })
+    .sort({'createdAt': -1})
+    .limit(limit)
+    .skip(offset)
+
+    return comments;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export const readCommentsInComment = async (commentId, offset, limit) => {
+  try {
+    const comments = await Comment.find({
+      parentCommentId:commentId
+    })
+    .sort({'createdAt': -1})
+    .limit(limit)
+    .skip(offset)
+
+    return comments;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export const readComment = async (commentId) => {
+  try {
+    const comment = await Comment.findById(commentId).lean()
+
+    return comment;
+  } catch (err) {
+    throw err;
+  }
+}
+
+
+export const creatCommentInPost = async (postId, userId, content) => {
+  try {
+    const comment = await Comment.create({
+        postId: postId,
+        userId: userId,
+        content: content,
+    });
+
+    return comment;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export const creatCommentInComment = async (postId, commentId, userId, content) => {
+  try {
+    const comment = await Comment.create({
+      postId:postId,
+      parentCommentId: ObjectId(commentId),
+      userId: userId,
+      content: content,
+    });
+
+    return comment;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export const updateComment = async (commentId, userId, content) => {
+  try {
+    const comments = await Comment.findByIdAndUpdate(commentId, {content:content}).lean();
+    console.log(comments);
+    return comments;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export const removeComment = async (commentId) => {
+  try {
+    const comments = await Comment.findByIdAndDelete(commentId).lean();
+
+    return comments;
+  } catch (err) {
+    throw err;
+  }
+}
+
